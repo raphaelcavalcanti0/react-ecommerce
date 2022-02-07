6f898dd2123f5e46a4b9a2ad0835cd7372660906
Raphael Cavalcanti <46363242+raphaelcavalcanti0@users.noreply.github.com>
2022-02-07 14:44:09 -0300

    Changed initial server setup to typescript

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { apiRoutes } from "./routes/ApiRoutes";
+
+const app = express();
+const port = 8000;
+
+// Setting Middleware to JSON formatted files
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(apiRoutes)
+
+// Starting server
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}/api/v1/products`);
+});
---
+++ server/src/routes/ApiRoutes.ts
@@ -0,0 +1,10 @@
+import { Router, Response, Request, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+
+export const apiRoutes = Router();
+
+apiRoutes.get('/api/v1/products',
+    async (req: Request, res: Response, next: NextFunction) => {
+        res.status(StatusCodes.OK).send({ products: [] })
+    })
